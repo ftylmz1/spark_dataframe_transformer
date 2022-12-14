f0fc1c05bd7acd43e575e386c8bff5137ca4a594
Fuat YILMAZ <fuat.yilmaz@external.danone.com>
2022-12-14 21:45:16 +0100

    third commit

--- tests/unit/test_schema.py
+++ tests/unit/test_schema.py
@@ -3,11 +3,6 @@ import pytest
 from spark_transformer.utilizers.spark_utilizer import first
 
 
-def check_data_type(dt, column, type):
-    filter(lambda x: x[0] == column) 
-
-
-
 def test_check_if_schema_exists():
     dt = [('id', 'bigint'), ('name', 'string'), ('surname', 'string')]
     r =  first(dt, 'id', 'bigint')
