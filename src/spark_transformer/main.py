3df4110c52e013ba6db2f5a21d1753d9e225ab68
Fuat YILMAZ <ftylmz1@gmail.com>
2022-12-14 22:37:53 +0100

    clean up

--- src/spark_transformer/main.py
+++ src/spark_transformer/main.py
@@ -8,16 +8,6 @@ from tranformations.executor import Executor
 from tranformations.drop import Drop
 from tranformations.hash import Hash
 
-
-import json
-from json import JSONEncoder
-try:
-    from types import SimpleNamespace as Namespace
-except ImportError:
-    # Python 2.x fallback
-    from argparse import Namespace
-
-
 spark = (SparkSession.builder
                      .master('local[*]')
                      .appName("base-test")
@@ -27,19 +17,11 @@ spark = (SparkSession.builder
 path = 'data/people.json'
 df = spark.read.json(path)
 
-
-
 executor = Executor(spark)
 hash_column = Hash('surname')
 drop_column = Drop('name')
-
 
 df = executor.apply(df, drop_column)
 df = executor.apply(df, hash_column)
 
 df.show(truncate=False)
-
-
-
-print('PySpark Version :'+spark.version)
-print('PySpark Version :'+spark.sparkContext.version)
