fdf661b171f163e91b6376449885ef1f17e6f8b9
Fuat YILMAZ <ftylmz1@gmail.com>
2022-12-15 13:39:49 +0100

    clean up

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 import setuptools
 from setuptools import setup, find_packages
 
-import sys, os
-
-sys.path.insert(0, os.path.dirname(__file__))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
-# this grabs the requirements from requirements.txt
-required_libs = {}
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines() if i.split("==")[0] in required_libs]
-
-
 setup(
     name='spark_transformer',
     version='0.0.1',
@@ -20,7 +9,7 @@ setup(
         "": ["*.json"],
     },
     install_requires=[
-        REQUIREMENTS
+        "jsonpickle"
     ],
     packages=find_packages(
         where='src'
